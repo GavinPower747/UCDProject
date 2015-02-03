221d61d57a1baf80f174d70f71be04d9e561eaf2
Gavin Power <gavinpower747@hotmail.com>
2015-02-03 15:07:09 +0000

    Initial Commit

---
+++ CodeTuts/CodeTuts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeTuts.Startup))]
+namespace CodeTuts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
